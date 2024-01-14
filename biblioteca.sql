0fb23affa936c48fd2e58b50ba165bba8e3ce6d5
sofia <sofiaduraes2001@gmail.com>
2024-01-14 10:42:52 +0000

    bas de dados

--- biblioteca.sql
+++ biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14-Jan-2024 às 00:18
+-- Tempo de geração: 14-Jan-2024 às 11:41
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.2.4
 
